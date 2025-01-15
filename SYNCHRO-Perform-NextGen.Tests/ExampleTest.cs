5bbf9421fc5526fc4c45f337b49bfb4c62fa2a43
Aaron Belsham <aaron.belsham@bentley.com>
2025-01-15 20:28:15 +1000

    Add Tests project, MSTest and Shouldly

---
+++ SYNCHRO-Perform-NextGen.Tests/ExampleTest.cs
@@ -0,0 +1,13 @@
+﻿using Shouldly;
+
+namespace SYNCHRO_Perform_NextGen.Tests;
+
+[TestClass]
+public sealed class ExampleTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        true.ShouldBe(true);
+    }
+}
