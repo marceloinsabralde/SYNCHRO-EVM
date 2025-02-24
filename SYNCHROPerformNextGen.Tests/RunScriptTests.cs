473cdbaf71724b2fd154724b97a9e7be0fc14843
Arun Malik <arun.malik@bentley.com>
2025-02-24 15:16:06 +1000

    Add Script Runner

---
+++ SYNCHROPerformNextGen.Tests/RunScriptTests.cs
@@ -0,0 +1,22 @@
+// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+using Shouldly;
+namespace SYNCHROPerformNextGen.Utilities;
+
+[TestClass]
+public class RunScriptTests
+{
+    [TestMethod]
+    public void Execute_SuccessfulScript_Succeeds()
+    {
+        var runScript = new RunScript();
+        runScript.Execute("bash", "-c", "echo test");
+    }
+
+    [TestMethod]
+    public void Execute_FailingScript_ThrowsExceptionWithErrorMessage()
+    {
+        var runScript = new RunScript();
+        var exception = Should.Throw<Exception>(() => runScript.Execute("bash", "-c", "echo test >&2; false"));
+        exception.Message.ShouldContain("test");
+    }
+}
---
+++ SYNCHROPerformNextGen.WebApi/Utilities/RunScript.cs
@@ -0,0 +1,38 @@
+// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+using System.Diagnostics;
+
+namespace SYNCHROPerformNextGen.Utilities;
+
+public class RunScript()
+{
+
+    public void Execute(params string[] arguments)
+    {
+        var processInfo = new ProcessStartInfo
+        {
+            FileName = arguments[0],
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        foreach (var arg in arguments.Skip(1).ToArray())
+        {
+            processInfo.ArgumentList.Add(arg);
+        }
+
+        using var process = new Process { StartInfo = processInfo };
+        process.Start();
+
+        string output = process.StandardOutput.ReadToEnd();
+        string error = process.StandardError.ReadToEnd();
+
+        process.WaitForExit();
+
+        if (process.ExitCode != 0)
+        {
+            throw new Exception($"Script execution failed with exit code {process.ExitCode}: {error}");
+        }
+    }
+}
