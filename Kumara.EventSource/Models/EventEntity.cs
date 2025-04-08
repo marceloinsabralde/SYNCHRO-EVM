532e841091e5adbfddf6c34b0262c8a6038328ad
Arun Malik <arun.malik@bentley.com>
2025-04-08 15:59:48 +1000

    Add EventEntity model

---
+++ Kumara.EventSource/Models/EventEntity.cs
@@ -0,0 +1,42 @@
+// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Kumara.EventSource.Models;
+
+public class EventEntity
+{
+    [Required]
+    [JsonPropertyName("iTwinGuid")]
+    public required Guid ITwinGuid { get; set; }
+
+    [Required]
+    [JsonPropertyName("accountGuid")]
+    public required Guid AccountGuid { get; set; }
+
+    [Required]
+    [JsonPropertyName("correlationId")]
+    public required string CorrelationId { get; set; }
+
+    [Required]
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; } = Guid.CreateVersion7();
+
+    [Required]
+    [JsonPropertyName("specVersion")]
+    public required string SpecVersion { get; set; }
+
+    [Required]
+    [JsonPropertyName("source")]
+    public required Uri Source { get; set; }
+
+    [Required]
+    [JsonPropertyName("type")]
+    public required string Type { get; set; }
+
+    [Required]
+    [JsonPropertyName("data")]
+    public JsonDocument DataJson { get; set; } = JsonDocument.Parse("{}");
+}
