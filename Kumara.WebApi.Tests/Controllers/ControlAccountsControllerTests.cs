f831ce73e04a76318f1847a2ef40c11e1db44389
Nate Kane <nathanaelkane@gmail.com>
2025-05-06 14:09:41 +1000

    Add initial ControlAccountsController

---
+++ Kumara.WebApi.Tests/Controllers/ControlAccountsControllerTests.cs
@@ -0,0 +1,120 @@
+// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+using System.Net;
+using System.Net.Http.Json;
+using Kumara.WebApi.Controllers.Responses;
+
+namespace Kumara.WebApi.Tests.Controllers;
+
+public sealed class ControlAccountsControllerTests : ControllerTestsBase
+{
+    [Fact]
+    public async Task Index_Success()
+    {
+        var iTwinId = Guid.CreateVersion7();
+        var otherITwinId = Guid.CreateVersion7();
+
+        // We're supplying a timestamp when we generate our UUIDs so we can control order
+        var timestamp = DateTimeOffset.UtcNow.AddDays(-7);
+
+        var controlAccount1 = Factories.ControlAccount(
+            id: Guid.CreateVersion7(timestamp.AddDays(0)),
+            iTwinId: iTwinId
+        );
+        var controlAccount2 = Factories.ControlAccount(
+            id: Guid.CreateVersion7(timestamp.AddDays(1)),
+            iTwinId: iTwinId
+        );
+        var otherITwinControlAccount = Factories.ControlAccount(
+            id: Guid.CreateVersion7(timestamp.AddDays(2)),
+            iTwinId: otherITwinId
+        );
+        await _dbContext.ControlAccounts.AddRangeAsync(
+            [controlAccount1, controlAccount2, otherITwinControlAccount],
+            TestContext.Current.CancellationToken
+        );
+        await _dbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
+
+        var response = await _client.GetAsync(
+            $"/api/v1/control-accounts?iTwinId={iTwinId}",
+            TestContext.Current.CancellationToken
+        );
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        var apiResponse = await response.Content.ReadFromJsonAsync<
+            ListResponse<ControlAccountResponse>
+        >(TestContext.Current.CancellationToken);
+        var controlAccounts = apiResponse?.items.ToList();
+
+        controlAccounts.ShouldNotBeNull();
+        controlAccounts.ShouldAllBe(controlAccount => controlAccount.ITwinId == iTwinId);
+        controlAccounts.Count().ShouldBe(2);
+        controlAccounts.ShouldBeEquivalentTo(
+            new List<ControlAccountResponse>
+            {
+                ControlAccountResponse.FromControlAccount(controlAccount1),
+                ControlAccountResponse.FromControlAccount(controlAccount2),
+            }
+        );
+    }
+
+    [Fact]
+    public async Task Index_WhenITwinIdMissing_BadRequest()
+    {
+        var response = await _client.GetAsync(
+            "/api/v1/control-accounts",
+            TestContext.Current.CancellationToken
+        );
+
+        await response.ShouldBeApiErrorBadRequest(
+            errorsPattern: @"{""iTwinId"":\[""The iTwinId field is required.""\]}"
+        );
+    }
+
+    [Fact]
+    public async Task Index_WhenITwinNotFound_NotFound()
+    {
+        var iTwinId = Guid.CreateVersion7();
+        var response = await _client.GetAsync(
+            $"/api/v1/control-accounts?iTwinId={iTwinId}",
+            TestContext.Current.CancellationToken
+        );
+
+        await response.ShouldBeApiErrorNotFound();
+    }
+
+    [Fact]
+    public async Task Show_Success()
+    {
+        var controlAccount = Factories.ControlAccount();
+        await _dbContext.ControlAccounts.AddAsync(
+            controlAccount,
+            TestContext.Current.CancellationToken
+        );
+        await _dbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
+
+        var response = await _client.GetAsync(
+            $"/api/v1/control-accounts/{controlAccount.Id}",
+            TestContext.Current.CancellationToken
+        );
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        var controlAccountResponse =
+            await response.Content.ReadFromJsonAsync<ControlAccountResponse>(
+                TestContext.Current.CancellationToken
+            );
+        controlAccountResponse.ShouldBeEquivalentTo(
+            ControlAccountResponse.FromControlAccount(controlAccount)
+        );
+    }
+
+    [Fact]
+    public async Task Show_WhenActivityNotFound_NotFound()
+    {
+        var response = await _client.GetAsync(
+            $"/api/v1/control-accounts/{Guid.NewGuid()}",
+            TestContext.Current.CancellationToken
+        );
+
+        await response.ShouldBeApiErrorNotFound();
+    }
+}
