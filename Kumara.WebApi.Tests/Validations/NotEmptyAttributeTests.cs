76c87fc45c4a5d50366eac8f3b86f0c79f51f7a7
Adam Hardiman <adam@bitrusher.com>
2025-05-12 13:09:52 +1000

    Add NotEmptyAttribute for validating a property is not empty

---
+++ Kumara.WebApi.Tests/Validations/NotEmptyAttributeTests.cs
@@ -0,0 +1,39 @@
+using Kumara.Validations;
+
+namespace Kumara.WebApi.Tests.Validations;
+
+public sealed class NotEmptyAttributeTests
+{
+    [Fact]
+    public void null_NotEmptyValidation()
+    {
+        var attr = new NotEmptyAttribute();
+
+        attr.IsValid(null).ShouldBeTrue();
+    }
+
+    [Fact]
+    public void String_NotEmptyValidation()
+    {
+        var attr = new NotEmptyAttribute();
+
+        attr.IsValid("").ShouldBeFalse();
+        attr.IsValid("foo").ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Guid_NotEmptyValidation()
+    {
+        var attr = new NotEmptyAttribute();
+
+        attr.IsValid(Guid.Empty).ShouldBeFalse();
+        attr.IsValid(Guid.CreateVersion7()).ShouldBeTrue();
+    }
+
+    [Fact]
+    public void UnsupportedType()
+    {
+        var attr = new NotEmptyAttribute();
+        Should.Throw<ArgumentException>(() => attr.IsValid(0));
+    }
+}
