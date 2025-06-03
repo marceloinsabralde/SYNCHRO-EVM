cf9ac76549ec8b75e1df1ae1ce3273f2f38d41a2
Jason Weathered <jason@jasoncodes.com>
2025-06-03 17:10:28 +1000

    Add test helper methods for getting/setting an entity's column values
    This allows us to test how custom properties persist to the database.

---
+++ Kumara.WebApi.Tests/DatabaseHelpers.cs
@@ -0,0 +1,61 @@
+// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Kumara.WebApi.Tests;
+
+public static class DatabaseHelpers
+{
+    public static IDictionary<string, object?> GetEntityColumnValues(
+        DbContext dbContext,
+        object entity
+    )
+    {
+        var entityType = dbContext.Model.FindEntityType(entity.GetType())!;
+
+        return entityType
+            .GetProperties()
+            .ToDictionary(
+                property => property.GetColumnName(),
+                property =>
+                    property switch
+                    {
+                        { PropertyInfo: not null } => property.PropertyInfo.GetValue(entity),
+                        { FieldInfo: not null } => property.FieldInfo.GetValue(entity),
+                        _ => throw new ArgumentException(
+                            $"Unknown property type for {property.GetColumnName()}"
+                        ),
+                    }
+            );
+    }
+
+    public static void SetEntityColumnValues(
+        DbContext dbContext,
+        object entity,
+        IDictionary<string, object?> values
+    )
+    {
+        var entityType = dbContext.Model.FindEntityType(entity.GetType())!;
+
+        var propertiesByColumnName = entityType
+            .GetProperties()
+            .ToDictionary(property => property.GetColumnName(), property => property);
+
+        foreach (var item in values)
+        {
+            var property = propertiesByColumnName[item.Key];
+
+            switch (property)
+            {
+                case { PropertyInfo: not null }:
+                    property.PropertyInfo.SetValue(entity, item.Value);
+                    break;
+                case { FieldInfo: not null }:
+                    property.FieldInfo.SetValue(entity, item.Value);
+                    break;
+                default:
+                    throw new ArgumentException($"Unknown property type for {item.Key}");
+            }
+        }
+    }
+}
