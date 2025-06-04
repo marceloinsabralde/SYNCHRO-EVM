4970393c3763180ebdefd064c76daa14bdc0fc6a
Jason Weathered <jason@jasoncodes.com>
2025-06-04 16:06:48 +1000

    Add check constraints to ensure nullness of field pairs is consistent

---
+++ Kumara.WebApi/Migrations/20250604055714_ConstrainActivityActualNullness.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kumara.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConstrainActivityActualNullness : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"
+                ALTER TABLE activities
+                ADD CONSTRAINT ck_activities_actual_start_has_time_nullness
+                CHECK ((actual_start IS NOT NULL) = (actual_start_has_time IS NOT NULL)),
+                ADD CONSTRAINT ck_activities_actual_finish_has_time_nullness
+                CHECK ((actual_finish IS NOT NULL) = (actual_finish_has_time IS NOT NULL));
+                "
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"
+                ALTER TABLE activities
+                DROP CONSTRAINT ck_activities_actual_start_has_time_nullness,
+                DROP CONSTRAINT ck_activities_actual_finish_has_time_nullness;
+                "
+            );
+        }
+    }
+}
