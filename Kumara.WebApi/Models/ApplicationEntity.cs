d89214f89ae43336cfef87231d181e43d281f314
Adam Hardiman <adam@bitrusher.com>
2025-06-05 16:14:24 +1000

    Introduce ApplicationEntity abstract class to handle common behaviour

---
+++ Kumara.WebApi/Models/ApplicationEntity.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+using System.Diagnostics.CodeAnalysis;
+using NodaTime;
+
+namespace Kumara.Models;
+
+public abstract class ApplicationEntity
+{
+    public Instant CreatedAt { get; set; }
+    public Instant UpdatedAt { get; set; }
+}
