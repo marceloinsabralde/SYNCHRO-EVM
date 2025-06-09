d10d1c425cb28670c788831b53bfe986a05a5c4b
Jason Weathered <jason@jasoncodes.com>
2025-06-09 12:11:11 +1000

    Add tool to configure mise env vars for JetBrains Rider's Test Runner
    The mise plugin at https://github.com/134130/intellij-mise does not
    currently support injecting environment variables into the Test Runner.
    It only works when you run the test project in its entirety.
    
    A workaround exists where you can manually configure env vars for the
    Test Runner in Settings > Build, Execution, Deployment > Unit Testing >
    Test Runner > Environment variables. This tool automates configuring
    environment variables using this workaround by writing directly
    to the XML file where these environment variables are stored.

---
+++ script/MiseRiderTestRunnerEnv/Program.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+using System.CommandLine;
+using System.Diagnostics;
+using System.Text.Json;
+
+static string GetPathFromArgs(string[] args)
+{
+    string? path = null;
+
+    var argument = new Argument<string>(
+        "path",
+        description: "path to the solution DotSettings.user file"
+    );
+    var command = new RootCommand("Populates Test Runner environment variables for JetBrains Rider")
+    {
+        argument,
+    };
+    command.SetHandler(
+        (string input) =>
+        {
+            path = input;
+        },
+        argument
+    );
+
+    var exitCode = command.Invoke(args);
+
+    if (path is null || exitCode != 0)
+    {
+        Environment.Exit(exitCode);
+    }
+
+    return path;
+}
+
+static IDictionary<string, string> GetMiseEnv()
+{
+    var processStartInfo = new ProcessStartInfo
+    {
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
+        UseShellExecute = false,
+        CreateNoWindow = true,
+    };
+
+    processStartInfo.FileName = "mise";
+    processStartInfo.ArgumentList.Add("env");
+    processStartInfo.ArgumentList.Add("--json");
+
+    using var process = new Process { StartInfo = processStartInfo };
+    process.Start();
+
+    string output = process.StandardOutput.ReadToEnd();
+    string error = process.StandardError.ReadToEnd();
+
+    process.WaitForExit();
+
+    if (process.ExitCode != 0)
+    {
+        throw new Exception(
+            $"`mise env` failed with exit code {process.ExitCode}: {output} {error}"
+        );
+    }
+
+    return JsonSerializer.Deserialize<IDictionary<string, string>>(output)!;
+}
+
+var path = GetPathFromArgs(args);
+var settings = new StringResourceDictionary();
+
+if (File.Exists(path))
+{
+    settings.Load(path);
+}
+
+var dirty = false;
+
+foreach (var env in GetMiseEnv())
+{
+    if (env.Key == "PATH")
+    {
+        continue;
+    }
+
+    var escapedKey = env.Key.Replace("_", "_005F").Replace("/", "_002F");
+    var settingKey =
+        $"/Default/Housekeeping/UnitTestingMru/UnitTestRunner/EnvironmentVariablesIndexed/={escapedKey}/@EntryIndexedValue";
+
+    if (settings[settingKey] != env.Value)
+    {
+        settings[settingKey] = env.Value;
+        dirty = true;
+    }
+}
+
+if (dirty)
+{
+    settings.Save(path);
+}
---
+++ script/MiseRiderTestRunnerEnv/StringResourceDictionary.cs
@@ -0,0 +1,92 @@
+// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+using System.Collections;
+using System.Xml;
+using System.Xml.Linq;
+
+class StringResourceDictionary
+{
+    private readonly XNamespace xml = "http://www.w3.org/XML/1998/namespace";
+    private readonly XNamespace wpf = "http://schemas.microsoft.com/winfx/2006/xaml/presentation";
+    private readonly XNamespace x = "http://schemas.microsoft.com/winfx/2006/xaml";
+    private readonly XNamespace s = "clr-namespace:System;assembly=mscorlib";
+
+    private XDocument document;
+
+    public StringResourceDictionary()
+    {
+        document = new XDocument(
+            new XElement(
+                wpf + "ResourceDictionary",
+                new XAttribute(XNamespace.Xmlns + "wpf", wpf),
+                new XAttribute(XNamespace.Xmlns + "x", x),
+                new XAttribute(XNamespace.Xmlns + "s", s)
+            )
+        );
+    }
+
+    public void Load(string path)
+    {
+        document = XDocument.Load(path);
+
+        NormalizeWhitespace();
+    }
+
+    public void Save(string path)
+    {
+        using (var stream = new StreamWriter(path))
+        {
+            var settings = new XmlWriterSettings
+            {
+                OmitXmlDeclaration = true,
+                Indent = true,
+                IndentChars = "\t",
+            };
+            using (var writer = XmlWriter.Create(stream, settings))
+            {
+                document.Save(writer);
+            }
+            stream.WriteLine();
+        }
+    }
+
+    public string? this[string key]
+    {
+        get
+        {
+            var element = GetStringElement(key);
+            return element?.Value;
+        }
+        set
+        {
+            var element = GetStringElement(key) ?? AddStringElement(key);
+            if (value is not null)
+            {
+                element.Value = value;
+            }
+            else
+            {
+                element.Remove();
+            }
+        }
+    }
+
+    private void NormalizeWhitespace()
+    {
+        document.Root?.Attribute(xml + "space")?.Remove();
+        document.Root?.DescendantNodes()?.OfType<XText>()?.Remove();
+    }
+
+    private XElement? GetStringElement(string key)
+    {
+        return document
+            .Root!.Elements(s + "String")
+            .FirstOrDefault(e => e.Attribute(x + "Key")?.Value == key);
+    }
+
+    private XElement AddStringElement(string key, string? value = null)
+    {
+        var element = new XElement(s + "String", new XAttribute(x + "Key", key), value);
+        document.Root!.Add(element);
+        return element;
+    }
+}
