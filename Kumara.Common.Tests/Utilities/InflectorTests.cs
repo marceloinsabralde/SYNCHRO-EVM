d33030cb3758180129f06f9121232cab26336743
Jason Weathered <jason@jasoncodes.com>
2025-06-11 17:25:53 +1000

    Add basic inflector utility class
    I couldn't find any existing libraries which handled the
    "Units of Measure" edge case so I wrote a wrapper for it.

---
+++ Kumara.Common.Tests/Utilities/InflectorTests.cs
@@ -0,0 +1,37 @@
+// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+
+using Kumara.Common.Utilities;
+
+namespace Kumara.Common.Tests.Utilities;
+
+public class InflectorTests
+{
+    [Theory]
+    [MemberData(nameof(PluralizeTestData))]
+    public void PluralizeTest(string singular, string plural)
+    {
+        Inflector.Pluralize(singular).ShouldBe(plural);
+        Inflector.Pluralize(plural).ShouldBe(plural);
+    }
+
+    [Theory]
+    [MemberData(nameof(PluralizeTestData))]
+    public void SingularizeTest(string singular, string plural)
+    {
+        Inflector.Singularize(singular).ShouldBe(singular);
+        Inflector.Singularize(plural).ShouldBe(singular);
+    }
+
+    public static TheoryData<string, string> PluralizeTestData =>
+        new TheoryData<string, string>
+        {
+            { "activity", "activities" },
+            { "Activity", "Activities" },
+            { "ControlAccount", "ControlAccounts" },
+            { "controlAccount", "controlAccounts" },
+            { "control account", "control accounts" },
+            { "UnitOfMeasure", "UnitsOfMeasure" },
+            { "unitOfMeasure", "unitsOfMeasure" },
+            { "Unit of Measure", "Units of Measure" },
+        };
+}
---
+++ Kumara.Common/Utilities/Inflector.cs
@@ -0,0 +1,37 @@
+// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+
+using System.Text.RegularExpressions;
+using Humanizer;
+
+namespace Kumara.Common.Utilities;
+
+public static class Inflector
+{
+    private static readonly Regex splitPattern = new Regex(@"(?<=[a-z])(?=[A-Z])|(?=\s)");
+    private static readonly Regex ofPattern = new Regex(@"\s?of", RegexOptions.IgnoreCase);
+
+    private static string Inflect(string value, Func<string, string> inflector)
+    {
+        var parts = splitPattern.Split(value);
+
+        var inflectIndex = parts.Length - 1;
+        if (parts.Length == 3 && ofPattern.IsMatch(parts[1]))
+        {
+            inflectIndex = 0;
+        }
+
+        parts[inflectIndex] = inflector(parts[inflectIndex]);
+
+        return string.Concat(parts);
+    }
+
+    public static string Pluralize(string value)
+    {
+        return Inflect(value, part => part.Pluralize());
+    }
+
+    public static string Singularize(string value)
+    {
+        return Inflect(value, part => part.Singularize());
+    }
+}
