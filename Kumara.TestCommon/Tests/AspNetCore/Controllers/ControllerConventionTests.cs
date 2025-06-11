fc684bd1e1c5f454e9b87ef00085e82f1b3b9890
Jason Weathered <jason@jasoncodes.com>
2025-06-11 11:27:05 +1000

    Add test to ensure all endpoint methods return typed responses

--- Kumara.TestCommon/Tests/AspNetCore/Controllers/ControllerConventionTests.cs
+++ Kumara.TestCommon/Tests/AspNetCore/Controllers/ControllerConventionTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 
 using System.Reflection;
+using Kumara.Common.Controllers.Responses;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Routing;
@@ -24,6 +25,50 @@ public sealed class ControllerConventionTests
         attribute.EndpointName.ShouldMatch(
             "^(List|Get|Create|Update)[A-Z]",
             $"{methodName} endpoint name should start with CRUD action"
+        );
+    }
+
+    [Theory]
+    [MemberData(nameof(EndpointTestData))]
+    public void EndpointsHaveTypedResults(string methodName, MethodInfo methodInfo)
+    {
+        var returnType = methodInfo.ReturnType;
+        if (returnType.IsGenericType && returnType.GetGenericTypeDefinition() == typeof(Task<>))
+        {
+            returnType = returnType.GetGenericArguments()[0];
+        }
+
+        if (returnType == typeof(IActionResult))
+        {
+            methodInfo
+                .GetCustomAttributes(typeof(ProducesResponseTypeAttribute))
+                .ShouldNotBeEmpty(
+                    $"{methodName} should return an ActionResult<> or be annotated with [ProducesResponseType]"
+                );
+            return;
+        }
+
+        var genericReturnType = returnType.IsGenericType
+            ? returnType.GetGenericTypeDefinition()
+            : returnType;
+        genericReturnType.ShouldBe(
+            typeof(ActionResult<>),
+            $"{methodName} should return an ActionResult<>"
+        );
+
+        var innerType = returnType.GetGenericArguments()[0];
+        var genericInnerType = innerType.IsGenericType
+            ? innerType.GetGenericTypeDefinition()
+            : innerType;
+        genericInnerType.ShouldBeOneOf(
+            new[]
+            {
+                typeof(ListResponse<>),
+                typeof(ShowResponse<>),
+                typeof(CreatedResponse),
+                typeof(UpdatedResponse),
+            },
+            $"{methodName} should return a ListResponse<>, ShowResponse<>, CreatedResponse, or UpdatedResponse"
         );
     }
 
