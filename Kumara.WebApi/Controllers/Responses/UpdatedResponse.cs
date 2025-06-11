482151920539d6237eb64cb1187a3f0721fc81c5
Jason Weathered <jason@jasoncodes.com>
2025-06-11 11:32:34 +1000

    Add UpdatedResponse type to wrap update responses in a top-level object
    This contains an Id to be consistent with the existing CreatedResponse

---
+++ Kumara.WebApi/Controllers/Responses/UpdatedResponse.cs
@@ -0,0 +1,8 @@
+// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+
+namespace Kumara.WebApi.Controllers.Responses;
+
+public class UpdatedResponse
+{
+    public required Guid Id { get; set; }
+}
