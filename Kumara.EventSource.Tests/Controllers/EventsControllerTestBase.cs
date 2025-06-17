87faa0a0e3b50aeb874c06fedd8f75ffa1482d81
Jason Weathered <jason@jasoncodes.com>
2025-06-17 12:15:27 +1000

    Use AppServicesHelper to create test Event Source app instances

--- Kumara.EventSource.Tests/Controllers/EventsControllerTestBase.cs
+++ Kumara.EventSource.Tests/Controllers/EventsControllerTestBase.cs
@@ -5,14 +5,14 @@ using Kumara.EventSource.Controllers;
 using Kumara.EventSource.Interfaces;
 using Kumara.EventSource.Repositories;
 using Kumara.EventSource.Utilities;
-using Microsoft.AspNetCore.Mvc.Testing;
+using Kumara.TestCommon.Helpers;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Kumara.EventSource.Tests.Controllers;
 
 public abstract class EventsControllerTestBase : IDisposable
 {
-    private readonly WebApplicationFactory<EventsController> _factory;
+    private readonly AppServicesHelper.AppFactory _factory;
     protected readonly HttpClient _client;
     protected readonly IEventRepository _eventRepository = new EventRepositoryInMemoryList();
     protected const string ApiBasePath = "/api/v1/events";
@@ -20,20 +20,11 @@ public abstract class EventsControllerTestBase : IDisposable
 
     protected EventsControllerTestBase()
     {
-        _factory = new WebApplicationFactory<EventsController>().WithWebHostBuilder(builder =>
+        _factory = AppServicesHelper.CreateWebApplicationFactory(builder =>
         {
             builder.ConfigureServices(services => services.AddSingleton(_eventRepository));
-            builder.UseSetting("https_port", "7104");
         });
-
-        _client = _factory.CreateClient(
-            new WebApplicationFactoryClientOptions
-            {
-                AllowAutoRedirect = false,
-                HandleCookies = true,
-                BaseAddress = new Uri("https://localhost:7104"),
-            }
-        );
+        _client = _factory.CreateClient();
     }
 
     public void Dispose()
