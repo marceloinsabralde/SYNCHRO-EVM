b6df7c175d88e29b9b15eaed7ae863b74ab0fbf9
Arun Malik <arun.malik@bentley.com>
2025-06-18 09:13:21 +1000

    Add Elasticsearch docker container

---
+++ .docker/elasticsearch/health.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+set -euo pipefail
+
+try_until="$(( $(date +%s) + 10 ))"
+while ! curl -fs -u "elastic:${ELASTIC_PASSWORD}" http://localhost:9200/_cluster/health > /dev/null; do
+  if [ "$(date +%s)" -ge "$try_until" ]; then
+    exit 1
+  fi
+  sleep 0.25
+done
