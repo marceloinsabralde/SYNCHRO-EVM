ea5db62546a92ac527b5480447303f285b46eea2
Jason Weathered <jason@jasoncodes.com>
2025-06-30 09:23:51 +1000

    Fix indentation

--- .docker/postgresql/initdb.d/extra-user-dbs.sh
+++ .docker/postgresql/initdb.d/extra-user-dbs.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 while IFS=":" read -r extra_name extra_password; do
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -v extra_name="$extra_name" -v extra_password="$extra_password" <<SQL
-  CREATE USER :"extra_name" ENCRYPTED PASSWORD :'extra_password';
-  CREATE DATABASE :"extra_name" WITH OWNER :"extra_name";
+    CREATE USER :"extra_name" ENCRYPTED PASSWORD :'extra_password';
+    CREATE DATABASE :"extra_name" WITH OWNER :"extra_name";
 SQL
 done < <(tr '|' '\n' <<<"$EXTRA_USER_DBS")
