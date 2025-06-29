2c6b7c0bcc6ccbc036cb73a145dbb0c89a015aa6
Jason Weathered <jason@jasoncodes.com>
2025-06-30 09:27:29 +1000

    Allow database name to be customised

--- .docker/postgresql/initdb.d/extra-user-dbs.sh
+++ .docker/postgresql/initdb.d/extra-user-dbs.sh
@@ -2,9 +2,14 @@
 # Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 set -euo pipefail
 
-while IFS=":" read -r extra_name extra_password; do
+while IFS=":" read -r extra_name extra_password extra_db; do
+  : ${extra_db:=$extra_name}
+
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -v extra_name="$extra_name" -v extra_password="$extra_password" <<SQL
     CREATE USER :"extra_name" ENCRYPTED PASSWORD :'extra_password';
-    CREATE DATABASE :"extra_name" WITH OWNER :"extra_name";
+SQL
+
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -v extra_name="$extra_name" -v extra_db="$extra_db" <<SQL
+    CREATE DATABASE :"extra_db" WITH OWNER :"extra_name";
 SQL
 done < <(tr '|' '\n' <<<"$EXTRA_USER_DBS")
