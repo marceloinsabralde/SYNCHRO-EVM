6c5623fa1b100cde07a7a236cd78a21a4649b8b3
Jason Weathered <jason@jasoncodes.com>
2025-07-02 16:13:30 +1000

    Add dummy controller to Kumara.Core to make TestCommon tests pass

---
+++ Kumara.Core/Controllers/DummyController.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+
+using Kumara.Common.Controllers.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kumara.Core.Controllers;
+
+[Route("api/v1/dummy")]
+[ApiController]
+public class DummyController : ControllerBase
+{
+    [HttpGet]
+    [EndpointName("GetDummy")]
+    [ProducesResponseType(typeof(void), StatusCodes.Status204NoContent)]
+    public IActionResult GetDummy()
+    {
+        return NoContent();
+    }
+}
