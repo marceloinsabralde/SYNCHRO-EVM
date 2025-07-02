e1669a139746fda032ec1873aea7440796bed816
Jason Weathered <jason@jasoncodes.com>
2025-07-02 15:49:42 +1000

    Use persistent Elasticsearch container for tests
    Running tests locally I am finding the ephemeral Elasticsearch
    contains using testcontainers to be quite slow to setup and teardown
    which makes running all tests a bit painful. Having a persistent
    container for running tests against is much nicer developer experience.
    
    This change saves about 20 seconds for the current test suite.

--- Kumara.Search.Tests/CollectionDefinitions.cs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-
-namespace Kumara.Search.Tests;
-
-[CollectionDefinition("IntegrationTests")]
-public class IntegrationTestCollection : ICollectionFixture<IntegrationTestAppFactory> { }
