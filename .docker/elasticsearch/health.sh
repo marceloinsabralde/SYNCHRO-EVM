3d22ca5298d5dbadbe0c03fb05129b896266d72c
Jason Weathered <jason@jasoncodes.com>
2025-07-03 10:12:57 +1000

    Increase health check wait time for Elasticsearch
    Elasticsearch typically takes around 10 seconds to start up locally.
    The default Docker timeout is 30 seconds, so let's just wait that long.

--- .docker/elasticsearch/health.sh
+++ .docker/elasticsearch/health.sh
@@ -2,10 +2,10 @@
 # Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 set -euo pipefail
 
-try_until="$(( $(date +%s) + 10 ))"
+try_until="$(( $(date +%s) + 30 ))"
 while ! curl -fs -u "elastic:${ELASTIC_PASSWORD}" http://localhost:9200/_cluster/health > /dev/null; do
   if [ "$(date +%s)" -ge "$try_until" ]; then
     exit 1
   fi
-  sleep 0.25
+  sleep 1
 done
