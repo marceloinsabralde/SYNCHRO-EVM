8d05e7036e835682e86cf44c759d5941f9c210ab
Adam Hardiman <adam@bitrusher.com>
2025-07-22 10:42:22 +1000

    Introduce ListMaterialsQuery

---
+++ Kumara.WebApi.Tests/Queries/ListMaterialsQueryTests.cs
@@ -0,0 +1,83 @@
+// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+
+using Kumara.WebApi.Models;
+using Kumara.WebApi.Queries;
+
+namespace Kumara.WebApi.Tests.Queries;
+
+public class ListMaterialsQueryTests : DatabaseTestBase
+{
+    public static Guid ITwinId = Guid.CreateVersion7();
+    public static List<Material> Materials = Enumerable
+        .Range(0, 10)
+        .Select(index =>
+        {
+            var timestamp = DateTimeOffset.UtcNow.AddDays(-index);
+            return Factories.Material(id: Guid.CreateVersion7(timestamp), iTwinId: ITwinId);
+        })
+        .OrderBy(material => material.Id)
+        .ToList();
+
+    public static Material OtherITwinMaterial = Factories.Material();
+
+    private async Task Setup()
+    {
+        await _dbContext.Materials.AddRangeAsync(
+            Materials.Concat([OtherITwinMaterial]),
+            TestContext.Current.CancellationToken
+        );
+        await _dbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
+    }
+
+    [Fact]
+    public async Task EmptyFilter_Test()
+    {
+        await Setup();
+
+        var queryResult = new ListMaterialsQuery(dbContext: _dbContext, iTwinId: ITwinId)
+            .ApplyFilter(new ListMaterialsQueryFilter())
+            .ExecuteQuery();
+
+        queryResult.Items.ShouldAllBe(material => material.ITwinId == ITwinId);
+        queryResult.Items.ShouldBeEquivalentTo(Materials);
+    }
+
+    [Fact]
+    public async Task ContinueFromId_Test()
+    {
+        await Setup();
+        var continueFromId = Materials.ElementAt(4).Id;
+
+        var queryResult = new ListMaterialsQuery(dbContext: _dbContext, iTwinId: ITwinId)
+            .ApplyFilter(new() { ContinueFromId = continueFromId })
+            .ExecuteQuery();
+
+        queryResult.Items.ShouldAllBe(material => material.ITwinId == ITwinId);
+        queryResult.Items.ShouldBeEquivalentTo(Materials.GetRange(5, 5));
+    }
+
+    [Fact]
+    public async Task Limit_Test()
+    {
+        await Setup();
+
+        var queryResult = new ListMaterialsQuery(dbContext: _dbContext, iTwinId: ITwinId)
+            .ApplyFilter(new())
+            .WithLimit(5)
+            .ExecuteQuery();
+
+        queryResult.Items.Count.ShouldBe(5);
+        queryResult.HasMore.ShouldBeTrue();
+        queryResult.Items.ShouldAllBe(material => material.ITwinId == ITwinId);
+        queryResult.Items.ShouldBeEquivalentTo(Materials.GetRange(0, 5));
+
+        queryResult = new ListMaterialsQuery(dbContext: _dbContext, iTwinId: ITwinId)
+            .ApplyFilter(new() { ContinueFromId = queryResult.LastReadId })
+            .WithLimit(5)
+            .ExecuteQuery();
+
+        queryResult.HasMore.ShouldBeFalse();
+        queryResult.Items.ShouldAllBe(material => material.ITwinId == ITwinId);
+        queryResult.Items.ShouldBeEquivalentTo(Materials.GetRange(5, 5));
+    }
+}
---
+++ Kumara.WebApi/Queries/ListMaterialsQuery.cs
@@ -0,0 +1,51 @@
+// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+
+using Kumara.Common.Queries;
+using Kumara.WebApi.Database;
+using Kumara.WebApi.Models;
+
+namespace Kumara.WebApi.Queries;
+
+public class ListMaterialsQuery
+    : IPageableQuery<ListMaterialsQuery, ListMaterialsQueryFilter, Material>
+{
+    private IQueryable<Material> _query;
+    private int _limit = 50;
+
+    public ListMaterialsQuery(ApplicationDbContext dbContext, Guid iTwinId)
+    {
+        _query = dbContext
+            .Materials.Where(material => material.ITwinId == iTwinId)
+            .OrderBy(material => material.Id);
+    }
+
+    public ListMaterialsQuery ApplyFilter(ListMaterialsQueryFilter filter)
+    {
+        if (filter.ContinueFromId is not null)
+            _query = _query.Where(material => material.Id > filter.ContinueFromId);
+
+        return this;
+    }
+
+    public ListMaterialsQuery WithLimit(int limit)
+    {
+        _limit = limit;
+        return this;
+    }
+
+    public QueryResult<Material> ExecuteQuery()
+    {
+        var items = _query.Take(_limit + 1).ToList();
+
+        bool hasMore = items.Count > _limit;
+        if (hasMore)
+            items.RemoveAt(items.Count - 1);
+
+        return new QueryResult<Material>() { Items = items, HasMore = hasMore };
+    }
+}
+
+public class ListMaterialsQueryFilter : IPageableQueryFilter
+{
+    public Guid? ContinueFromId { get; set; }
+}
