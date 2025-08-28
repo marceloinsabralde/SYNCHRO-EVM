34b395dba7facc4f47eb9302b5edb25602ede88d
Nate Kane <nathanaelkane@gmail.com>
2025-08-28 12:44:47 +1000

    Make events table distributed (with citus)

---
+++ Kumara.EventSourceToo.Tests/Database/CitusTests.cs
@@ -0,0 +1,60 @@
+// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+
+using Kumara.EventSourceToo.Models;
+using Kumara.EventSourceToo.Tests.Factories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kumara.EventSourceToo.Tests.Database;
+
+public sealed class CitusTests : DatabaseTestBase
+{
+    [Fact]
+    public async Task DistributesEventsAcrossShardsByAccountId()
+    {
+        var accountId1 = Guid.CreateVersion7();
+        var event1 = EventFactory.CreateActivityCreatedV1Event(accountId: accountId1);
+        var event2 = EventFactory.CreateActivityCreatedV1Event(accountId: accountId1);
+
+        var accountId2 = Guid.CreateVersion7();
+        var event3 = EventFactory.CreateActivityCreatedV1Event(accountId: accountId2);
+
+        await _dbContext.Events.AddRangeAsync(
+            [event1, event2, event3],
+            TestContext.Current.CancellationToken
+        );
+
+        await _dbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
+
+        var shardId1 = await GetShardId(accountId1);
+        var shard1Events = await GetEventsForShard(shardId1);
+
+        shard1Events.ShouldContain(event1);
+        shard1Events.ShouldContain(event2);
+
+        var shardId2 = await GetShardId(accountId2);
+        var shard2Events = await GetEventsForShard(shardId2);
+
+        shard2Events.ShouldContain(event3);
+    }
+
+    private record CitusShard(int Id);
+
+    private async Task<int> GetShardId(Guid iTwinId, string table = "events")
+    {
+        var row = await _dbContext
+            .Database.SqlQuery<CitusShard>(
+                $"SELECT get_shard_id_for_distribution_column({table}, {iTwinId}) AS id"
+            )
+            .FirstAsync(TestContext.Current.CancellationToken);
+        return row.Id;
+    }
+
+    private async Task<List<Event>> GetEventsForShard(int shardId)
+    {
+        var sql = $"SELECT * FROM events_{shardId}";
+        var rows = await _dbContext
+            .Events.FromSqlRaw(sql)
+            .ToListAsync(TestContext.Current.CancellationToken);
+        return rows;
+    }
+}
---
+++ Kumara.EventSourceToo/Migrations/20250828024333_CreateDistributedEvents.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kumara.EventSourceToo.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDistributedEvents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("SELECT create_distributed_table('events', 'account_id')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("SELECT undistribute_table('events')");
+        }
+    }
+}
