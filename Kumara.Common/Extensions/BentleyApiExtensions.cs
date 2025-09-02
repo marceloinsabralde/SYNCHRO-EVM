33feb8cb65b73ff1c79616b15036f02f6941106c
Jonathon M. Abbott <jonathon.abbott@bentley.com>
2025-09-02 18:48:48 +1000

    Authenticate Swagger & Swagger UI

--- Kumara.Common/Extensions/BentleyApiExtensions.cs
+++ Kumara.Common/Extensions/BentleyApiExtensions.cs
@@ -36,4 +36,32 @@ public static class BentleyApiExtensions
 
         return builder;
     }
+
+    // Configures authentication for the Swagger UI and JSON.
+    //
+    // Provides a browser OIDC flow so unauthenticated Users can access.
+    // Allows access to the `.json` file via a shared secret to facilitate Scorecard Checklists.
+    public static WebApplicationBuilder ConfigureBentleySwaggerAuthentication(
+        this WebApplicationBuilder builder
+    )
+    {
+        var swaggerLoginAuthentication =
+            builder.RegisterConfig<SwaggerLoginAuthenticationOptions>();
+        builder.RegisterConfig<SwaggerLoginMiddlewareOptions>();
+
+        builder
+            .Services.AddAuthentication()
+            .AddSwaggerLoginAuthentication(o =>
+            {
+                o.Authority = swaggerLoginAuthentication.Authority;
+                o.ClientId = swaggerLoginAuthentication.ClientId;
+                o.CallbackPath = swaggerLoginAuthentication.CallbackPath;
+            });
+
+        return builder;
+    }
+
+    // Adds the SwaggerLoginMiddleware to actually enforce Authentication and initiate the browser-OIDC flow
+    public static void UseBentleySwaggerAuthentication(this IApplicationBuilder app) =>
+        app.UseContextualMiddleware<ContextualSwaggerLoginMiddleware>();
 }
